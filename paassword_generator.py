415a50a8e2afebe2375f2cd68b843e26d7bce95e
otega99 <83589929+otega99@users.noreply.github.com>
2021-05-04 07:54:06 +0100

    Add files via upload

---
+++ paassword_generator.py
@@ -0,0 +1,27 @@
+import string
+import random
+
+#this array named characters contains the lowercase letters,
+#uppercase letters and the numbers 0 to 9 each as items in the list
+characters=[string.ascii_lowercase,
+            string.ascii_uppercase,
+            [str(i) for i in range(10)]]
+
+def generate_password():
+    #the password variable is initialized as an empty string
+    password=''
+    for i in range(10):
+        #here we choose an item in the list characters
+        value=characters[random.randint(0, 2)]
+        #here we choose a character or digit from the item chosen above 
+        password+=value[random.randint(0, len(value)-1)]
+    return password
+    
+passwords=[generate_password() for i in range(10)]
+
+#this for loop will generate ten random passwords 
+#using the generate_password() function
+for password in passwords:
+    print(password)
+    
+    
