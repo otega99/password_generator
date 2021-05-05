c65adb744e48613eed54af55bc59347f557b464c
otega99 <83589929+otega99@users.noreply.github.com>
2021-05-05 11:07:51 +0100

    removed comments
    Removed coments and changed variable names

--- paassword_generator.py
+++ paassword_generator.py
@@ -1,27 +1,18 @@
 import string
 import random
 
-#this array named characters contains the lowercase letters,
-#uppercase letters and the numbers 0 to 9 each as items in the list
 characters=[string.ascii_lowercase,
             string.ascii_uppercase,
             [str(i) for i in range(10)]]
 
 def generate_password():
-    #the password variable is initialized as an empty string
     password=''
     for i in range(10):
-        #here we choose an item in the list characters
-        item=characters[random.randint(0, 2)]
-        #here we choose a character or digit from the item chosen above 
-        password+=item[random.randint(0, len(item)-1)]
+        character_group=characters[random.randint(0, 2)]
+        password+=character_group[random.randint(0, len(character_group)-1)]
     return password
     
 passwords=[generate_password() for i in range(10)]
 
-#this for loop will generate ten random passwords 
-#using the generate_password() function
 for password in passwords:
     print(password)
-    
-    
